6ef1bb95b37ee34967e45c71cfb0be507f7e72a2
Mohamed Ayoub Neggaz <>
2026-04-13 20:40:13 +0200

    Fix stone screen production data assignment

--- wereld1/actions/stone.php
+++ wereld1/actions/stone.php
@@ -3,12 +3,12 @@ if($ACTIONS_MASSIVKEY_HIGHAAASSDD != "sdjahsdkJHSAJDKHALKJHSADJHSADNsjdhaksjdlhJ
 	exit;
 }
 
-$iron_datas['stone_production'] = floor($arr_production[$village['stone']]*$config['speed'])/60;
+$stone_datas['stone_production'] = floor($arr_production[$village['stone']]*$config['speed'])/60;
 
-if($village['stone'] == $cl_builds->get_maxstage("iron")){
- $iron_datas['stone_production_next'] = false;
+if($village['stone'] == $cl_builds->get_maxstage("stone")){
+ $stone_datas['stone_production_next'] = false;
 }else{
- $iron_datas['stone_production_next'] = floor($arr_production[$village['stone']+1]*$config['speed'])/60;
+ $stone_datas['stone_production_next'] = floor($arr_production[$village['stone']+1]*$config['speed'])/60;
 }
 
 $tpl->assign("stone_datas", $stone_datas);
